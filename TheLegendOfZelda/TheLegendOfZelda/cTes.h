20fb958f68ecfc820109f16b5febd2c97f38090a
5thingol <guillem.cordoba@gmail.com>
2015-11-13 01:06:16 +0100

    Acompanyant implementat!!

---
+++ TheLegendOfZelda/TheLegendOfZelda/cTes.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "cPlayer.h"
+
+#define TIME 50*10
+
+class cTes : public cPlayer {
+private:
+	int BFS(Tile* map);
+	int time = -1;
+	int move;
+
+public:
+	cTes();
+	~cTes();
+
+	void Draw(int tex_id);
+	void Logic(Tile* map);
+	void Logic(Tile* map, std::vector<cBicho*> Enemies);
+};
